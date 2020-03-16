614b952ee8688ec39f8592b1aebc27ee5d6debc5
DIEGO GUSTAVO COSTA <diego.g.costa@caixa.gov.br>
2020-03-16 13:05:15 -0300

    Início do desenvolvimento - Batalha Naval

--- jogos.py
+++ jogos.py
@@ -1,12 +1,13 @@
 import forca
 import adivinhacao
+import bn
 
 def escolhe_jogo():
     print("*********************************")
     print("*******Escolha o seu jogo!*******")
     print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação (3) Batalha Naval")
 
     jogo = int(input("Qual jogo? "))
 
@@ -16,6 +17,9 @@ def escolhe_jogo():
     elif(jogo == 2):
         print("Jogando adivinhação")
         adivinhacao.jogar()
+    elif(jogo == 3):
+        print("Jogando batalha naval")
+        bn.jogar()
 
 if(__name__ == "__main__"):
     escolhe_jogo()
