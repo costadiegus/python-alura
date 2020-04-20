1e8f4783873c4a9db4b3d6bc8a253037bbd7f3d8
DIEGO GUSTAVO COSTA <diego.g.costa@caixa.gov.br>
2020-04-20 18:29:06 -0300

    OO Avancado: 03 Classe sem herança

--- oo-avancado/modelos.py
+++ oo-avancado/modelos.py
@@ -41,19 +41,35 @@ class Serie(Programa):
         return f"Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}"
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
+
+
 def executa():
     vingadores = Filme("vingadores - guerra infinita", 2018, 160)
     atlanta = Serie("atlanta", 2018, 2)
+    tmep = Filme("todo mundo em panico", 1999, 100)
+    demolidor = Serie("demolidor", 2016, 2)
 
     vingadores.dar_likes()
     vingadores.dar_likes()
     vingadores.dar_likes()
     atlanta.dar_likes()
     atlanta.dar_likes()
+    tmep.dar_likes()
+    tmep.dar_likes()
+    demolidor.dar_likes()
+    demolidor.dar_likes()
 
-    listinha = [atlanta, vingadores]
+    listinha = [atlanta, vingadores, demolidor, tmep]
+    minha_playlist = Playlist("fim de semana", listinha)
 
-    for programa in listinha:
+    for programa in minha_playlist.programas:
         print(programa)
 
 
